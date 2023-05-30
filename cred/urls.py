4af310d031ef56539d1a927e817b50f78567c229
austindotz <austindot456@gmail.com>
2023-05-30 23:33:26 +0530

    "initial commit"

---
+++ cred/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path,include
+
+urlpatterns = [
+
+    path('reg',views.reg,name = 'reg'),
+    path('login',views.login,name = 'login'),
+    path('logout', views.logout, name='logout')
+
+
+
+]
